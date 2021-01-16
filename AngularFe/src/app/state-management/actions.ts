27ae8e0106b8f443d48b4e2bba8f20d9e43b29e2
evgdim <evgeny.d.dimitrov@gmai.com>
2021-01-17 00:13:33 +0200

    replace old angular with angular 11 empty template

--- AngularFe/src/app/state-management/actions.ts
+++
@@ -1,3 +0,0 @@
-export const LOGIN_ATEMPT = 'LOGIN_ATEMPT';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_ERROR = 'LOGIN_ERROR';
--- AngularFe/src/app/state-management/login.action.ts
+++
@@ -1,5 +0,0 @@
-import { Action } from '@ngrx/store';
-import { Login } from '../model/login';
-export class LoginAction implements Action {
-    constructor(public type: string, public payload: any) {}
-}
